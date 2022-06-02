194829eabc82bc9827bc2955480aeaeefb028856
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-02 11:52:48 -0700

    add constant methods

---
+++ Illegible_Cms_V2/Shared/BasicShared/Constants/ConstantMethods/HashId.cs
@@ -0,0 +1,38 @@
+﻿using HashidsNet;
+
+namespace Illegible_Cms_V2.Shared.BasicShared.Constants.ConstantMethods
+{
+    public static class HashId
+    {
+        private const string HashSalt =
+            "amard-lJa2faL25HFJsfjag812fFkGh123aFR9tG67gDha3r4#vgwsrKfjHdHbKhg6J2KWe";
+        private const int HashLength = 12;
+        private const string HashAlphabets = "abcdefghklmnoprstuvw123456789";
+
+        private static readonly Hashids Hasher =
+            new Hashids(HashSalt, HashLength, HashAlphabets);
+
+        public static string Encode(this int id)
+        {
+            return Hasher.Encode(id);
+        }
+
+        public static string Encode(this long id)
+        {
+            return Hasher.EncodeLong(id);
+        }
+
+        public static int Decode(this string eid)
+        {
+            try
+            {
+                return Hasher.Decode(eid)[0];
+            }
+            catch
+            {
+                return -1;
+                throw new ArgumentException("Invalid encoded Id value");
+            }
+        }
+    }
+}
---
+++ Illegible_Cms_V2/Shared/BasicShared/Constants/Language.cs
@@ -0,0 +1,8 @@
+﻿namespace Illegible_Cms_V2.Shared.BasicShared.Constants
+{
+    public enum Language
+    {
+        English = 1,
+        Dutch = 2
+    }
+}
