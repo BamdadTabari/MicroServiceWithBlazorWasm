9fe37a3f4cd8c934e33a9927e97d120ac0c72cb9
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-02 11:47:10 -0700

    add constants

---
+++ Illegible_Cms_V2/Shared/BasicShared/Costants/Language.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Illegible_Cms_V2.Shared.BasicShared.Costants
+{
+    public enum Language
+    {
+        English = 1,
+        Dutch = 2
+    }
+}
