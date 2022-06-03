588fea01c10df4fe43925781663b25d6f74ddda6
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-03 14:02:59 -0700

    add validition helper

---
+++ Illegible_Cms_V2/Shared/Helpers/ValidationHelper.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System.Linq;
+
+namespace Illegible_Cms_V2.Shared.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static string? GetFirstErrorMessage(this ValidationResult result)
+        {
+            return result.Errors.FirstOrDefault()?.ErrorMessage;
+        }
+
+        public static object? GetFirstErrorState(this ValidationResult result)
+        {
+            return result.Errors.FirstOrDefault()?.CustomState;
+        }
+    }
+}
