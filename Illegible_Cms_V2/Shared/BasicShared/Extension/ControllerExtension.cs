e2a24e8898d2eb3037a957a94157061e15cc8fc6
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-04 03:38:15 -0700

    cleaning

--- Illegible_Cms_V2/Shared/BasicShared/Extension/ControllerExtension.cs
+++ Illegible_Cms_V2/Shared/BasicShared/Extension/ControllerExtension.cs
@@ -5,20 +5,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace Illegible_Cms_V2.Shared.BasicShared.Extension
 {
 
-    public static IActionResult ReturnResponse(this ControllerBase controller, OperationResult operation)
+    public static class ControllerExtension
     {
-        var response = operation.Value;
-        if (response is ErrorModel errorModel)
-            response = new ErrorResponse(errorModel);
-
-        return operation.Status switch
+        public static IActionResult ReturnResponse(this ControllerBase controller, OperationResult operation)
         {
-            OperationResultStatus.Ok => controller.Ok(response),
-            OperationResultStatus.Invalidated => controller.BadRequest(response),
-            OperationResultStatus.NotFound => controller.NotFound(response),
-            OperationResultStatus.Unauthorized => controller.UnprocessableEntity(response),
-            OperationResultStatus.Unprocessable => controller.UnprocessableEntity(response),
-            _ => controller.UnprocessableEntity(response)
-        };
+            object response = operation.Value;
+            if (response is ErrorModel errorModel)
+                response = new ErrorResponse(errorModel);
+
+            return operation.Status switch
+            {
+                OperationResultStatus.Ok => controller.Ok(response),
+                OperationResultStatus.Invalidated => controller.BadRequest(response),
+                OperationResultStatus.NotFound => controller.NotFound(response),
+                OperationResultStatus.Unauthorized => controller.UnprocessableEntity(response),
+                OperationResultStatus.UnProcessable => controller.UnprocessableEntity(response),
+                _ => controller.UnprocessableEntity(response)
+            };
+        }
     }
 }
