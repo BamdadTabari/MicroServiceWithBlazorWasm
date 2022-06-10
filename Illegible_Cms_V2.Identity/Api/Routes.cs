395f838d588e1c7a48a6d42f8119e3a05ec1e229
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-10 07:52:46 -0700

    add folder and api routes

---
+++ Illegible_Cms_V2.Identity/Api/Routes.cs
@@ -0,0 +1,15 @@
+﻿namespace Illegible_Cms_V2.Identity.Api
+{
+    public static class Routes
+    {
+        public const string Api = "api/";
+
+        public const string Auth = Api + "auth/";
+
+        public const string Users = Api + "users/";
+
+        public const string Role = Api + "role/";
+
+        public const string Permission = Api + "permission/";
+    }
+}
