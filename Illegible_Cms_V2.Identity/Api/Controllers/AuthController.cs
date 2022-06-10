046ba8229c6f487111b905ba17fa115856d2527b
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-10 08:59:54 -0700

    add auth login user action (repoes not added)

---
+++ Illegible_Cms_V2.Identity/Api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Illegible_Cms_V2.Identity.Api.Models.Requests.Auth;
+using Illegible_Cms_V2.Identity.Api.ResultFilters.Auth;
+using Illegible_Cms_V2.Identity.Application.Models.Commands.Auth;
+using Illegible_Cms_V2.Shared.BasicShared.Extension;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Illegible_Cms_V2.Identity.Api.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Routes.Auth + "login")]
+        [LoginResultFilter]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var operation = await _mediator.Send(new LoginCommand
+            {
+                UserName = request.Username,
+                Password = request.Password,
+            });
+
+            return this.ReturnResponse(operation);
+        }
+    }
+}
---
+++ Illegible_Cms_V2.Identity/Api/ResultFilters/Auth/LoginResultFilter.cs
@@ -0,0 +1,25 @@
+﻿using Illegible_Cms_V2.Identity.Application.Models.Results.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Illegible_Cms_V2.Identity.Api.ResultFilters.Auth
+{
+    public class LoginResultFilter: ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var result = context.Result as ObjectResult;
+
+            if (result?.Value is LoginResult value)
+                result.Value = new
+                {
+                    Username = value.UserName,
+                    FullName = value.FullName,
+                    AccessToken = value.AccessToken,
+                    RefreshToken = value.RefreshToken,
+                };
+
+            await next();
+        }
+    }
+}
