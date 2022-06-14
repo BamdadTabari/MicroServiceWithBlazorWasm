502dfd65db5bd5f858fd4595d12bfd5922e41789
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-14 09:29:33 -0700

    add user interface and repo and fiters for user and like that

---
+++ Illegible_Cms_V2.Identity/Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Illegible_Cms_V2.Identity.Application.Interfaces.Repositories;
+
+namespace Illegible_Cms_V2.Identity.Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        //IRoleRepository Roles { get; }
+        //IClaimRepository Claims { get; }
+        //IPermissionRepository Permissions { get; }
+
+        Task<bool> CommitAsync();
+    }
+}
---
+++ Illegible_Cms_V2.Identity/Application/Models/Filters/Users/UserIncludes.cs
@@ -0,0 +1,7 @@
+﻿namespace Illegible_Cms_V2.Identity.Application.Models.Filters.Users
+{
+    public class UserIncludes
+    {
+        public bool Role { get; set; }
+    }
+}
