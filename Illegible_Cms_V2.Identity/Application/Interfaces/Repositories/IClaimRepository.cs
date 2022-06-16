2c250eda99893b89d31f2e4e32bc72b7b9a25e87
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-16 02:19:25 -0700

    add repo intrfaces

---
+++ Illegible_Cms_V2.Identity/Application/Interfaces/Repositories/IClaimRepository.cs
@@ -0,0 +1,13 @@
+﻿using Illegible_Cms_V2.Identity.Application.Models.Filters.Claims;
+using Illegible_Cms_V2.Identity.Domain.Claims;
+
+namespace Illegible_Cms_V2.Identity.Application.Interfaces.Repositories
+{
+    public interface IClaimRepository : IRepository<Claim>
+    {
+        Task<Claim> GetClaimByIdAsync(int id);
+        Task<List<Claim>> GetClaimsByIdsAsync(IEnumerable<int> ids);
+        Task<List<Claim>> GetClaimsByFilterAsync(ClaimFilter filter);
+        Task<int> CountClaimsByFilterAsync(ClaimFilter filter);
+    }
+}
---
+++ Illegible_Cms_V2.Identity/Application/Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Illegible_Cms_V2.Identity.Application.Models.Filters.Roles;
+using Illegible_Cms_V2.Identity.Domain.Roles;
+
+namespace Illegible_Cms_V2.Identity.Application.Interfaces.Repositories
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+        Task<Role> GetRoleByIdAsync(int id);
+        Task<List<Role>> GetRolesByIdsAsync(IEnumerable<int> ids);
+        Task<List<Role>> GetRolesByFilterAsync(RoleFilter filter);
+        Task<int> CountRolesByFilterAsync(RoleFilter filter);
+    }
+}
