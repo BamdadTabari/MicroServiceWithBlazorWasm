8ac5d5e5f59b63f2d0fe92463b7b41f1cd057120
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-17 11:07:57 -0700

    when you find out you forgot so many things

---
+++ Illegible_Cms_V2.Identity/Api/Extensions/DependencyInjection/RedisCacheInjection.cs
@@ -0,0 +1,17 @@
+﻿using Illegible_Cms_V2.Shared.BasicShared.Configurations;
+
+namespace Illegible_Cms_V2.Identity.Api.Extensions.DependencyInjection
+{
+    public static class RedisCacheInjection
+    {
+        public static IServiceCollection AddConfiguredRedisCache(this IServiceCollection services, IConfiguration configuration)
+        {
+            var config = configuration.GetSection(RedisCacheConfig.Key).Get<RedisCacheConfig>();
+
+            // Distributed caching
+            //services.AddStackExchangeRedis("identity", config);
+
+            return services;
+        }
+    }
+}
