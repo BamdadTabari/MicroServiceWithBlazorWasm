78745c0b09e00fedf2a4184aad4164f850a8aa24
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-17 02:21:03 -0700

    add repositories

---
+++ Illegible_Cms_V2.Identity/Persistence/Repositories/Claims/ClaimRepository.cs
@@ -0,0 +1,61 @@
+﻿using Illegible_Cms_V2.Identity.Application.Interfaces.Repositories;
+using Illegible_Cms_V2.Identity.Application.Models.Filters.Claims;
+using Illegible_Cms_V2.Identity.Domain.Claims;
+using Illegible_Cms_V2.Identity.Persistence.Extensions;
+using Illegible_Cms_V2.Shared.BasicShared.Extension;
+using Microsoft.EntityFrameworkCore;
+
+namespace Illegible_Cms_V2.Identity.Persistence.Repositories.Claims
+{
+    public class ClaimRepository : Repository<Claim>, IClaimRepository
+    {
+        private readonly IQueryable<Claim> _queryable;
+
+        public ClaimRepository(AppDbContext context) : base(context)
+        {
+            _queryable = DbContext.Set<Claim>();
+        }
+
+        public async Task<Claim> GetClaimByIdAsync(int id)
+        {
+            return await _queryable
+                .SingleOrDefaultAsync(x => x.Id == id) ?? new Claim();
+        }
+
+        public async Task<List<Claim>> GetClaimsByIdsAsync(IEnumerable<int> ids)
+        {
+            var query = _queryable;
+
+            query = query.AsNoTracking();
+
+            // Filter by ids
+            if (ids?.Any() == true)
+                query = query.Where(x => ids.Contains(x.Id));
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<List<Claim>> GetClaimsByFilterAsync(ClaimFilter filter)
+        {
+            var query = _queryable;
+
+            query = query.AsNoTracking();
+
+            query = query.ApplyFilter(filter);
+            query = query.ApplySort(filter.SortBy);
+
+            return await query.Paginate(filter.Page, filter.PageSize).ToListAsync();
+        }
+
+        public async Task<int> CountClaimsByFilterAsync(ClaimFilter filter)
+        {
+            var query = _queryable;
+
+            query = query.AsNoTracking();
+
+            query = query.ApplyFilter(filter);
+
+            return await query.CountAsync();
+        }
+    }
+}
---
+++ Illegible_Cms_V2.Identity/Persistence/Repositories/Permissions/PermissionRepository.cs
@@ -0,0 +1,65 @@
+﻿using Illegible_Cms_V2.Identity.Application.Interfaces.Repositories;
+using Illegible_Cms_V2.Identity.Application.Models.Filters.Permissions;
+using Illegible_Cms_V2.Identity.Domain.Permissions;
+using Illegible_Cms_V2.Identity.Persistence.Extensions;
+using Illegible_Cms_V2.Shared.BasicShared.Extension;
+using Microsoft.EntityFrameworkCore;
+
+namespace Illegible_Cms_V2.Identity.Persistence.Repositories.Permissions
+{
+    public class PermissionRepository : Repository<Permission>, IPermissionRepository
+    {
+        private readonly IQueryable<Permission> _queryable;
+
+        public PermissionRepository(AppDbContext context) : base(context)
+        {
+            _queryable = DbContext.Set<Permission>();
+        }
+
+        public async Task<Permission> GetPermissionByIdAsync(int id)
+        {
+            return await _queryable
+                .Include(x => x.Roles)
+                .SingleOrDefaultAsync(x => x.Id == id) ?? new Permission();
+        }
+
+        public async Task<List<Permission>> GetPermissionsByIdsAsync(IEnumerable<int> ids)
+        {
+            var query = _queryable;
+
+            query = query.AsNoTracking()
+                .Include(x => x.Roles);
+
+            // Filter by ids
+            if (ids?.Any() == true)
+                query = query.Where(x => ids.Contains(x.Id));
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<List<Permission>> GetPermissionsByFilterAsync(PermissionFilter filter)
+        {
+            var query = _queryable;
+
+            query = query.AsNoTracking()
+                .Include(x => x.Roles); ;
+
+            query = query.ApplyFilter(filter);
+            query = query.ApplySort();
+
+            return await query.Paginate(filter.Page, filter.PageSize).ToListAsync();
+        }
+
+        public async Task<int> CountPermissionsByFilterAsync(PermissionFilter filter)
+        {
+            var query = _queryable;
+
+            query = query.AsNoTracking()
+                .Include(x => x.Roles);
+
+            query = query.ApplyFilter(filter);
+
+            return await query.CountAsync();
+        }
+    }
+}
