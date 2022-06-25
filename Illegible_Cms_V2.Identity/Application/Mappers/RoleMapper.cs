cd9e7f926409181db5b960f935d20e0682bbbdbb
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-25 13:08:21 -0700

    making love with c# in depth

--- Illegible_Cms_V2.Identity/Application/Mappers/RoleMapper.cs
+++ Illegible_Cms_V2.Identity/Application/Mappers/RoleMapper.cs
@@ -1,21 +1,31 @@
 ﻿using Illegible_Cms_V2.Identity.Application.Models.Base.Permissions;
 using Illegible_Cms_V2.Identity.Application.Models.Base.Roles;
+using Illegible_Cms_V2.Identity.Application.Models.Base.Users;
 using Illegible_Cms_V2.Identity.Domain.Roles;
-
 namespace Illegible_Cms_V2.Identity.Application.Mappers
 {
     public static class RoleMapper
     {
         public static RoleModel MapToRoleModel(this Role role)
         {
-            var permission = role.RolePermission?.Select(x => x.Permission)
-                .MapToPermissionModels().ToList();
+            //var permission = role.RolePermission?.Select(x => x.Permission)
+            //    .MapToPermissionModels().ToList();
             return new RoleModel
             {
                 Id = role.Id,
                 Name = role.Name,
                 Title = role.Title,
-                Permissions = permission ?? new List<PermissionModel>()
+                Permissions = role?.RolePermission as List<PermissionModel> ?? new List<PermissionModel>(),
+                RolePermission = role?.RolePermission as List<RolePermissionModel> ?? new List<RolePermissionModel>(),
+                CreatedAt = role != null ? role.CreatedAt : DateTime.Now,
+                IsArchived = role != null && role.IsArchived,
+                Creator = role != null ? role.Creator.MapToUserModel() : new UserModel(),
+                CreatorId = role != null ? role.CreatorId : 0,
+                IsDeleted = role != null && role.IsDeleted,
+                UpdatedAt = role != null ? role.UpdatedAt : DateTime.Now,
+                UpdaterId = role != null ? role.UpdaterId : 0,
+                Updater = role != null ? role.Updater.MapToUserModel() : new UserModel(),
+               // UserRoles = role !=null ? role.UserRoles.MapTo : new List<UserRoleModel>(),
             };
         }
 
--- Illegible_Cms_V2.Identity/Application/Models/Base/Roles/RoleModel.cs
+++ Illegible_Cms_V2.Identity/Application/Models/Base/Roles/RoleModel.cs
@@ -1,22 +1,25 @@
 ﻿using Illegible_Cms_V2.Identity.Application.Models.Base.Permissions;
+using Illegible_Cms_V2.Identity.Application.Models.Base.Users;
 
 namespace Illegible_Cms_V2.Identity.Application.Models.Base.Roles
 {
     public class RoleModel
     {
         public int Id { get; set; }
-
-        public string Title { get; set; }
         public string Name { get; set; }
-        public List<PermissionModel> Permissions { get; set; }
+        public string Title { get; set; }
 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-
         public int CreatorId { get; set; }
         public int UpdaterId { get; set; }
+        public bool IsArchived { get; set; }
+        public bool IsDeleted { get; set; }
 
-        public string CreatorName { get; set; }
-        public string UpdaterName { get; set; }
+        public ICollection<UserRoleModel> UserRoles { get; set; }
+        public ICollection<RolePermissionModel> RolePermission { get; set; }
+        public UserModel Creator { get; set; }
+        public UserModel Updater { get; set; }
+        public List<PermissionModel> Permissions { get; internal set; }
     }
 }
