0b1aeb316de1b51a880a634be4be0d268d0cf36c
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-25 12:35:11 -0700

    find a way for nested mapping

--- Illegible_Cms_V2.Identity/Application/Mappers/UserMapper.cs
+++ Illegible_Cms_V2.Identity/Application/Mappers/UserMapper.cs
@@ -1,6 +1,7 @@
 ﻿using Illegible_Cms_V2.Identity.Application.Helpers;
 using Illegible_Cms_V2.Identity.Application.Models.Base.Roles;
 using Illegible_Cms_V2.Identity.Application.Models.Base.Users;
+using Illegible_Cms_V2.Identity.Application.Models.Base.Claims;
 using Illegible_Cms_V2.Identity.Domain.Users;
 
 namespace Illegible_Cms_V2.Identity.Application.Mappers
@@ -18,11 +19,24 @@ namespace Illegible_Cms_V2.Identity.Application.Mappers
                 IsEmailConfirmed = user.IsEmailConfirmed,
                 State = user.State,
                 IsLockedOut = user.IsLockedOut(),
-
                 CreatedAt = user.CreatedAt,
                 UpdatedAt = user.UpdatedAt,
-
-                UserRoles = (ICollection<UserRole>)(roles ?? new List<RoleModel>())
+                ConcurrencyStamp = user.ConcurrencyStamp,
+                FailedLoginCount = user.FailedLoginCount,
+                IsDeleted = user.IsDeleted,
+                IsMobileConfirmed = user.IsMobileConfirmed,
+                LastLoginDate = user.LastLoginDate,
+                Claims = (ICollection<ClaimModel>)user.Claims,
+                CreatorId = user.CreatorId,
+                LastPasswordChangeTime = user.LastPasswordChangeTime,
+                LockoutEndTime = user.LockoutEndTime,
+                Mobile = user.Mobile,
+                PasswordHash = user.PasswordHash,
+                SecurityStamp = user.SecurityStamp,
+                UpdaterId = user.UpdaterId,
+                Creator = MapToUserModel(user.Creator),
+                Updater = MapToUserModel(user.Updater),
+                UserRoles = (ICollection<UserRoleModel>)(ICollection<UserRole>)(roles ?? new List<RoleModel>())
             };
         }
 
