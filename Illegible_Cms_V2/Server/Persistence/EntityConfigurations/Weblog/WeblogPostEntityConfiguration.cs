ea14d6e7a288451bd84fc024a1ede7ccbd974fe7
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-06-30 07:04:53 -0700

    claening andconfigs part one

--- Illegible_Cms_V2/Server/Persistence/EntityConfigurations/Weblog/WeblogPostEntityConfiguration.cs
+++ Illegible_Cms_V2/Server/Persistence/EntityConfigurations/Weblog/WeblogPostEntityConfiguration.cs
@@ -1,10 +1,11 @@
-﻿using Illegible_Cms_V2.Shared.BasicShared.Constants;
+﻿using Illegible_Cms_V2.Server.Domain.Weblog;
+using Illegible_Cms_V2.Shared.BasicShared.Constants;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Illegible_Cms_V2.Server.Domain.Weblog
+namespace Illegible_Cms_V2.Server.Persistence.EntityConfigurations.Weblog
 {
-    internal class WeblogPostEntityConfiguration: IEntityTypeConfiguration<WeblogPost>
+    internal class WeblogPostEntityConfiguration : IEntityTypeConfiguration<WeblogPost>
     {
         public void Configure(EntityTypeBuilder<WeblogPost> builder)
         {
