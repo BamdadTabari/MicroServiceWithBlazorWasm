6eee218abe0cb6319a73208a433e3a5c46deec6c
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-07-02 12:53:07 -0700

    add so

---
+++ Illegible_Cms_V2/Server/Application/Helpers/Common/ValidationHelper.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+
+namespace Illegible_Cms_V2.Server.Application.Helpers.Common
+{
+    public static class ValidationHelper
+    {
+        public static string GetFirstErrorMessage(this ValidationResult result)
+        {
+            return result.Errors.FirstOrDefault()?.ErrorMessage ?? string.Empty;
+        }
+
+        public static object GetFirstErrorState(this ValidationResult result)
+        {
+            return result.Errors.FirstOrDefault()?.CustomState ?? string.Empty;
+        }
+    }
+}
---
+++ Illegible_Cms_V2/Server/Application/Helpers/Weblog/WeblogPostHelper.cs
@@ -0,0 +1,15 @@
+﻿using Illegible_Cms_V2.Server.Application.Models.Commands.Weblog;
+using Illegible_Cms_V2.Server.Domain.Weblog;
+
+namespace Illegible_Cms_V2.Server.Application.Helpers.Weblog
+{
+    public static class WeblogPostHelper
+    {
+        private static WeblogPost AddWeblogPost(CreateWeblogPostCommand command) => new WeblogPost()
+        {
+            Title = command.Title,
+            Summery = command.Summery,
+            TextContent = command.TextContent
+        };
+    }
+}
