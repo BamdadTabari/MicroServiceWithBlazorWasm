1c2b2d385a02b8bb5c7bf748b7c5ef544c797cc7
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-08-10 07:25:51 -0700

    snake index my first ill components

---
+++ Illegible_Cms_V2/Client/Pages/IllComponents/SnakeIndex.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Illegible_Cms_V2.Client.Pages.IllComponents;
+
+public partial class SnakeIndex
+{
+    [Parameter]
+    public string? TextItemOne { get; set; }
+
+    [Parameter]
+    public string? TextItemTwo { get; set; }
+
+    [Parameter]
+    public string? TextItemThree { get; set; }
+
+    [Parameter]
+    public string? TextItemFour { get; set; }
+}
