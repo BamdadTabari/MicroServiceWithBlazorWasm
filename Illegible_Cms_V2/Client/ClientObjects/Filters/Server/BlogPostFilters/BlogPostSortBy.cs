b5eeb44250069326c8e0708ac08f1f5e9d19d9ce
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-08-13 12:06:50 -0700

    REQS AND FILS FOR POST

---
+++ Illegible_Cms_V2/Client/ClientObjects/Filters/Server/BlogPostFilters/BlogPostSortBy.cs
@@ -0,0 +1,7 @@
+﻿namespace Illegible_Cms_V2.Client.ClientObjects.Filters.Server.BlogPostFilters;
+
+public enum BlogPostSortBy
+{
+    CreationDate = 12,
+    CreationDateDescending = 13,
+}
---
+++ Illegible_Cms_V2/Client/ClientObjects/Requests/Server/BlogPostCategoryRequests/UpdateBlogPostCategoryRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Illegible_Cms_V2.Client.ClientObjects.Requests.Server.BlogPostCategoryRequests;
+
+public class UpdateBlogPostCategoryRequest
+{
+    public string CategoryTitle { get; set; }
+    public string CategoryIcon { get; set; }
+}
